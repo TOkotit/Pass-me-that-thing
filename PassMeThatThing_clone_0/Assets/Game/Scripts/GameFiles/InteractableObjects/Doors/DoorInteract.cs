a7bdc59986fd224dc0150a5274dcc7bf50a4d297
TOkotit <105845895+TOkotit@users.noreply.github.com>
2026-04-27 21:44:57 +0500

    Сделал дверь бункера и просто двери

---
+++ PassMeThatThing_clone_0/Assets/Game/Scripts/GameFiles/InteractableObjects/Doors/DoorInteract.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using Mirror;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Game.Scripts.GameFiles.InteractableObjects.Doors
+{
+    public class DoorInteract : NetworkBehaviour
+    {
+        [Header("Movement")]
+        [SerializeField] private float closedYRotation = 0f;
+        [SerializeField] private float openYRotation = 90f;
+        [SerializeField] private float moveSpeed = 2f;
+
+        [SyncVar(hook = nameof(OnOpenStateChanged))]
+        private bool isOpen;
+        
+        
+        private float targetRotationY;
+        private bool initialized;
+
+        public override void OnStartServer()
+        {
+            base.OnStartServer();
+            isOpen = false;
+            targetRotationY = closedYRotation;
+            initialized = true;
+        }
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+
+            targetRotationY = isOpen ? openYRotation : closedYRotation;
+            initialized = true;
+        }
+
+        private void FixedUpdate()
+        {
+            if (!initialized)
+                return;
+
+            var rotation = transform.localEulerAngles;
+            var newY = Mathf.MoveTowardsAngle(rotation.y, targetRotationY, moveSpeed * Time.fixedDeltaTime);
+
+            if (Mathf.Approximately(rotation.y, newY)) return;
+
+            rotation.y = newY;
+            transform.localEulerAngles = rotation;
+        }
+
+        public void ToggleFromClient()
+        {
+            if (!isLocalPlayer)
+                return;
+
+            CmdToggleDoor();
+        }
+
+        [Command]
+        private void CmdToggleDoor() => isOpen = !isOpen;
+
+        [Server]
+        public void OpenDoor() => isOpen = true;
+
+        [Server]
+        public void CloseDoor() => isOpen = false;
+
+        private void OnOpenStateChanged(bool oldValue, bool newValue)
+        {
+            targetRotationY = newValue ? openYRotation : closedYRotation;
+        }
+        
+        // Отладочный метод для того, чтобы смотреть работу без интеракции
+        // [ServerCallback]
+        // private void Update()
+        // {
+        //     if (Time.time % 5f < 0.02f)
+        //     {
+        //         isOpen = !isOpen;
+        //         Debug.Log($"[SERVER] Gate state: {isOpen}");
+        //     }
+        // }
+        
+    }
+}
