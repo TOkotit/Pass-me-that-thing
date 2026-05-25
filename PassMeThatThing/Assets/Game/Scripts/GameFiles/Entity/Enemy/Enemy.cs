c78e0d8c2cc797763435dcb7cafa3159e12c9144
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-05-25 22:19:06 +0500

    enemy model

--- PassMeThatThing/Assets/Game/Scripts/GameFiles/Entity/Enemy/Enemy.cs
+++ PassMeThatThing/Assets/Game/Scripts/GameFiles/Entity/Enemy/Enemy.cs
@@ -1,23 +1,50 @@
+using DI;
+using Entity;
 using Game.Scripts.GameFiles.Entity.Enemy.EnemyFSM;
+using Game.Scripts.GameFiles.Items;
 using Mirror;
 using UnityEngine;
 using UnityEngine.AI;
+using VContainer;
+using VContainer.Unity;
 
 namespace Game.Scripts.GameFiles.Entity.Enemy
 {
-    public class Enemy : NetworkBehaviour
+    public class Enemy : Damagable
     {
+        [Inject]
+        private DamagableRegistry _damagableRegistry;
+        
+        
+        public override DamagableModel DamagableModel
+        {
+            get => EnemyModel;
+        }
+
+        public EnemyModel EnemyModel;
+        
         protected EnemyStateMachine stateMachine;
         
         [SerializeField] protected TargetDetector targetDetector;
         [SerializeField] protected EnemyMovementController movementController;
         [SerializeField] protected EnemyAttackController attackController;
         
+        public override void OnStartClient()
+        {
+            LifetimeScope.Find<GameplayScope>().Container.Inject(this);
+
+            EnemyModel = new EnemyModel();
+            
+            _damagableRegistry.Register(this);
+        }
+        
         protected void Awake()
         {
             stateMachine = new EnemyStateMachine();
             
         }
+
+        
 
         protected void Start()
         {
