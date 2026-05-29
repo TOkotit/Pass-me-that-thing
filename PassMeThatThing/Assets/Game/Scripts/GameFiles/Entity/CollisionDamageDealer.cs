69322ff33756d93e8d64dd687b6cf9158280689b
Sp1ke <97825185+SpikeTheSpider@users.noreply.github.com>
2026-05-29 22:56:07 +0500

    CollisionDamageDealer

--- PassMeThatThing/Assets/Game/Scripts/GameFiles/Entity/CollisionDamageDealer.cs
+++ PassMeThatThing/Assets/Game/Scripts/GameFiles/Entity/CollisionDamageDealer.cs
@@ -1,19 +1,39 @@
 using System;
 using Entity;
-using Game.Scripts.GameFiles.Items.ItemPhysics;
 using Mirror;
 using UnityEngine;
 
 namespace Game.Scripts.GameFiles.Entity
 {
-    public class CollisionDamageDealer : MonoBehaviour
+    public class CollisionDamageDealer : NetworkBehaviour
     {
+        [SerializeField] private int damage = 10;
+        [SerializeField] private bool useVelocityDamage = false;
+        [SerializeField] private float velocityDamageMultiplier = 1f;
+        [SerializeField] private float cooldown = 0.5f;
+
+        private float _lastDamageTime = -999f;
+
         private void OnCollisionEnter(Collision other)
         {
-            if (DamagableRegistry.Instance.TryGetDamagable(other.gameObject, out var damagable))
+            if (!isServer) return;               
+            if (DamagableRegistry.Instance == null) return;
+
+            if (!DamagableRegistry.Instance.TryGetDamagable(other.gameObject, out var damagable))
+                return;
+
+            if (Time.time - _lastDamageTime < cooldown)
+                return;
+
+            var finalDamage = damage;
+            if (useVelocityDamage)
             {
-                
+                var velocity = other.relativeVelocity.magnitude;
+                finalDamage += (int)(velocity * velocityDamageMultiplier);
             }
+
+            damagable.DamagableModel.HealthPool.TakeDamage(finalDamage);
+            _lastDamageTime = Time.time;
         }
     }
 }
