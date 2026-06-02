46ae6713b3ac10e847b31189b87a7f2b0e706d18
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-06-03 01:36:33 +0500

    теперь с mainmenu запускать!!!!

--- PassMeThatThing/Assets/Game/Scripts/GameFiles/MainMenu/View/UI/ScreenMainMenu/ScreenMainMenuBinder.cs
+++ PassMeThatThing/Assets/Game/Scripts/GameFiles/MainMenu/View/UI/ScreenMainMenu/ScreenMainMenuBinder.cs
@@ -1,51 +1,65 @@
 ﻿using Game.UI;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Button = UnityEngine.UI.Button;
 
 namespace Game.MainMenu.View.UI.ScreenMainMenu
 {
     public class ScreenMainMenuBinder : WindowBinder<ScreenMainMenuViewModel>
     {
-        [SerializeField] private Button _btnPlay;
-        [SerializeField] private Button _btnContinue;
+        [SerializeField] private Button _btnHost;
+        [SerializeField] private Button _btnJoin;
         [SerializeField] private Button _btnOptions;
         [SerializeField] private Button _btnExit;
+        
+        [SerializeField] private InputField _ipText;
 
 
         private void Start()
         {
-            _btnPlay?.onClick.AddListener(OnPlayButtonClicked);
-            _btnContinue?.onClick.AddListener(OnContinueButtonClicked);
+            _btnHost?.onClick.AddListener(OnHostButtonClicked);
+            _btnJoin?.onClick.AddListener(OnJoinButtonClicked);
             _btnOptions?.onClick.AddListener(OnOptionsButtonClicked);
             _btnExit?.onClick.AddListener(OnExitButtonClicked);
+            
+            _ipText?.onValueChanged.AddListener(OnIpTextChanged);
         }
-
+        
         private void OnDestroy()
         {
-            _btnPlay?.onClick.RemoveListener(OnPlayButtonClicked);
-            _btnContinue?.onClick.RemoveListener(OnContinueButtonClicked);
+            _btnHost?.onClick.RemoveListener(OnHostButtonClicked);
+            _btnJoin?.onClick.RemoveListener(OnJoinButtonClicked);
             _btnOptions?.onClick.RemoveListener(OnOptionsButtonClicked);
             _btnExit?.onClick.RemoveListener(OnExitButtonClicked);
+            
+            _ipText?.onValueChanged.RemoveListener(OnIpTextChanged);
         }
 
-        private void OnPlayButtonClicked()
+        private void OnHostButtonClicked()
         {
-            ViewModel.RequestPlay();
+            ViewModel.RequestHost();
         }
         
-        private void OnContinueButtonClicked()
+        private void OnJoinButtonClicked()
         {
-            
+            ViewModel.RequestJoin();
         }
-        
+
         private void OnOptionsButtonClicked()
         {
             
         }
+
         
         private void OnExitButtonClicked()
         {
             
+        }
+
+        private void OnIpTextChanged(string value)
+        {
+            ViewModel.RequestIpAddress(value);
         }
         
     }
