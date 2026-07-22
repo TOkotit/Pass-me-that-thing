a453b733c367d5ba349d05185057969e4243782b
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-07-22 20:56:41 +0500

    ui toolkit screen craft

--- PassMeThatThing/Assets/Game/Scripts/GameFiles/Gameplay/View/UI/ScreenCraftMenu/ScreenCraftMenuBinder.cs
+++ PassMeThatThing/Assets/Game/Scripts/GameFiles/Gameplay/View/UI/ScreenCraftMenu/ScreenCraftMenuBinder.cs
@@ -1,101 +1,151 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Game.Scripts.Enums;
 using Game.Scripts.GameFiles.Entity.Buildings.Misc;
 using Game.UI;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
+// using Button = UnityEngine.UI.Button;
+// using Image = UnityEngine.UI.Image;
 
 namespace Game.Gameplay.View.UI.ScreenBuild
 {
     public class ScreenCraftMenuBinder : WindowBinder<ScreenCraftMenuViewModel>
     {
-        [SerializeField] private RecipeViewElement recipeViewPrefab;
-        [SerializeField] private GameObject recipesContainer;
+        // [SerializeField] private RecipeViewElement recipeViewPrefab;
+        // [SerializeField] private GameObject recipesContainer;
+        //
+        // [SerializeField] private Image resultImage;
+        // [SerializeField] private TextMeshProUGUI resultText;
+        // [SerializeField] private Button craftButton;
+        //
+        // [SerializeField] private ResourceViewElement resourceViewPrefab;
+        // [SerializeField] private GameObject resourceContainer;
+
+        private ResourceDatabase _resourceDatabase;
+        private List<WorkbenchItemRecipe> _recipesData = new();
         
-        [SerializeField] private Image resultImage;
-        [SerializeField] private TextMeshProUGUI resultText;
-        [SerializeField] private Button craftButton;
+        private WorkbenchItemRecipe _selectedRecipe;
         
-        [SerializeField] private ResourceViewElement resourceViewPrefab;
-        [SerializeField] private GameObject resourceContainer;
+        [SerializeField] private UIDocument uiDocument;
+        [SerializeField] private VisualTreeAsset recipeViewPrefab;
+        [SerializeField] private VisualTreeAsset recipeResViewPrefab;
+        [SerializeField] private VisualTreeAsset resourceViewPrefab;
         
-        private List<RecipeViewElement> _recipes = new();
-        
-        private RecipeViewElement _selectedRecipe;
+        private VisualElement _root;
+        private ListView _recipeContainer;
+        private VisualElement _resourceContainer;
+
+        private VisualElement _resultImg;
+        private Label _resultText;
+        private Button _craftButton;
+
+        private void Awake()
+        {
+            _root = uiDocument.rootVisualElement;
+
+            _recipeContainer = _root.Q<ListView>("RecipesContainer");
+            _resourceContainer = _root.Q<VisualElement>("ResContainer");
+
+            _resultImg = _root.Q<VisualElement>("CurrentResultIm");
+            _resultText = _root.Q<Label>("CurrentResultLb");
+            _craftButton = _root.Q<Button>("CraftBtn");
+            
+        }
         
         private void Start()
         {
             ViewModel.RequestUpdateRecipes(UpdateRecipes);
             ViewModel.RequestAvailableResources(UpdateResources);
-            craftButton.onClick.AddListener(OnCraftClick);
+
+            InitRecipesList();
+            
+            _recipeContainer.selectedIndicesChanged += OnRecipeClick;
+            
+            _craftButton.RegisterCallback<ClickEvent>(OnCraftClick);
         }
 
         private void OnDestroy()
         {
-            craftButton.onClick.RemoveListener(OnCraftClick);
-            
-            foreach (var recipeViewElement in _recipes)
-            {
-                recipeViewElement.OnClick -= OnRecipeClick;
-            }
+            _recipeContainer.selectedIndicesChanged -= OnRecipeClick;
+            _craftButton.UnregisterCallback<ClickEvent>(OnCraftClick);
         }
 
         public void UpdateResources(IReadOnlyDictionary<Resource,int> resources, ResourceDatabase resourceDatabase)
         {
             foreach (var r in resources)
             {
-                var res = Instantiate(resourceViewPrefab, resourceContainer.transform);
-                
                 var rData = resourceDatabase.GetResource(r.Key);
+                
+                var res = resourceViewPrefab.Instantiate();
+                _resourceContainer.Add(res);
 
-
-                res.resImage.sprite = rData.resourceImage;
-                res.resNameText.text = rData.resourceType.ToString();
-                res.resAmountText.text = r.Value.ToString();
+                res.Q<VisualElement>("ResImg").style.backgroundImage = new StyleBackground(rData.resourceImage);
+                res.Q<Label>("NameLb").text = rData.resourceType.ToString();
+                res.Q<Label>("AmountLb").text = r.Value.ToString();
             }
         }
 
-        public void UpdateRecipes(List<WorkbenchItemRecipe> recipes, ResourceDatabase resourceDatabase)
+        private void InitRecipesList()
         {
-            foreach (var r in recipes)
+            _recipeContainer.makeItem = () => recipeViewPrefab.Instantiate();
+
+            _recipeContainer.bindItem = (element, index) =>
             {
-                var recipeViewElement = Instantiate(recipeViewPrefab, recipesContainer.transform);
+                var r = _recipesData[index];
+
+                element.Q<VisualElement>("ResultIm").style.backgroundImage 
+                    = new StyleBackground(r.Item.ItemImage);
+                element.Q<Label>("ResultLb").text = r.Item.Id;
                 
-                recipeViewElement.OnClick += OnRecipeClick;
-                recipeViewElement.RecipeId = r.recipeId;
+                var recipeResContainer = element.Q<GroupBox>("RecipeResContainer");
                 
-                _recipes.Add(recipeViewElement);
-                
-                var resourcesData = new List<(Sprite, string)>();
                 foreach (var rp in r.Resources)
                 {
-                    var rData = resourceDatabase.GetResource(rp.resource);
-                    resourcesData.Add((rData.resourceImage, rp.amount.ToString()));
+                    var rData = _resourceDatabase.GetResource(rp.resource);
+                    
+                    var rRes = recipeResViewPrefab.Instantiate();
+                    recipeResContainer.Add(rRes);
+                    
+                    rRes.Q<VisualElement>("ResIm").style.backgroundImage 
+                        = new StyleBackground(rData.resourceImage);
+                    rRes.Q<Label>("ResLb").text = rp.amount.ToString();
                 }
-                
-                recipeViewElement.SetData(r.Item.ItemImage, r.Item.Id, resourcesData);
-            }
-        }
-
-        public void UpdateResultInfo(RecipeViewElement recipeViewElement)
-        {
-            resultImage.sprite = recipeViewElement.ResultImage.sprite;
-            resultText.text = recipeViewElement.ResultText.text;
-        }
-
-        public void OnRecipeClick(RecipeViewElement recipeViewElement, PointerEventData data)
-        {
-            _selectedRecipe = recipeViewElement;
+            };
             
-            UpdateResultInfo(_selectedRecipe);
+            _recipeContainer.itemsSource = _recipesData;
+            _recipeContainer.fixedItemHeight = 200;
+        }
+        
+        public void UpdateRecipes(List<WorkbenchItemRecipe> recipes, ResourceDatabase resourceDatabase)
+        {
+            _resourceDatabase = resourceDatabase;
+            foreach (var r in recipes)
+            {
+                _recipesData.Add(r);
+            }
+            _recipeContainer.Rebuild();
+        }
+        
+        public void UpdateResultInfo(Sprite sprite, string text)
+        {
+            _resultImg.style.backgroundImage = new StyleBackground(sprite);
+            _resultText.text = text;
         }
 
-        public void OnCraftClick()
+        public void OnRecipeClick(IEnumerable<int> index)
         {
-            ViewModel.RequestCraft(_selectedRecipe.RecipeId);
+            _selectedRecipe = _recipesData[index.First()];
+            
+            UpdateResultInfo(_selectedRecipe.Item.ItemImage, _selectedRecipe.Item.ItemName);
+        }
+
+        public void OnCraftClick(ClickEvent e)
+        {
+            ViewModel.RequestCraft(_selectedRecipe.recipeId);
         }
         
     }
