acca5e5be2c7c9dbf9489da8fa65d59c673b7f42
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-07-26 15:22:51 +0500

    уборка от канвасного options screen

--- PassMeThatThing/Assets/Game/Scripts/GameFiles/MainMenu/View/UI/ScreenOptionsMenu/OptionsAudioType.cs
+++
@@ -1,9 +0,0 @@
-namespace Game.Scripts.GameFiles.MainMenu.View.UI.ScreenOptionsMenu
-{
-    public enum OptionsAudioType
-    {
-        All,
-        Music,
-        SFX
-    }
-}
--- PassMeThatThing/Assets/Game/Scripts/GameFiles/MainMenu/View/UI/ScreenOptionsMenu/ScreenOptionsBinder.cs
+++ PassMeThatThing/Assets/Game/Scripts/GameFiles/MainMenu/View/UI/ScreenOptionsMenu/ScreenOptionsBinder.cs
@@ -5,7 +5,6 @@ using Ami.BroAudio;
 using Assets.Game.Scripts.GameFiles.MainMenu.View.UI.ScreenOptionsMenu;
 using AYellowpaper.SerializedCollections;
 using Enums;
-using Game.Scripts.GameFiles.MainMenu.View.UI.ScreenOptionsMenu;
 using Game.Scripts.Systems;
 using Game.UI;
 using TMPro;
@@ -17,37 +16,15 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
 {
     public class ScreenOptionsBinder : WindowBinder<ScreenOptionsViewModel>
     {
-        //[SerializeField] private Button btnClose;
-        //[SerializeField] private Button btnSave;
-        //[Header("Settings Pages")]
-        //[SerializeField] private SerializedDictionary<SettingsPage, GameObject> settingsPages;
-        //[SerializeField] private SerializedDictionary<SettingsPage, Button> btnSettingsPages;
-        
-        //[Header("Audio Settings")]
-        //[SerializeField] private SerializedDictionary<BroAudioType, Slider> audioSliders;
-        
-        //[Header("Video Settings")]
-        //[SerializeField] private TMP_Dropdown resolutionsDropdown;
-        //[SerializeField] private Toggle fullscreenToggle;
-        
-        //[Header("Keyboard Settings")]
-        //[SerializeField] private RebindObject rebindButtonPrefab;
-        //[SerializeField] private GameObject rebindBackground;
-        //[SerializeField] private GameObject scrollView;
-
-        //private Dictionary<RebindObject, InputAction> _inputActionReferences = new();
-
-        private Dictionary<InputMapType, List<InputAction>>  _inputActions;
-        private List<RebindButton> _rebindButtons = new();
-
         [SerializeField] private UIDocument uiDocument;
-        //[SerializeField] private VisualTreeAsset rebindPrefab;
 
         private VisualElement _root;
         private Button _closeBtn;
         private Button _saveBtn;
         private VisualElement _rebindBlockImage;
+
         //gameSettings
+        //
 
         //audio
         private Slider _allSlider;
@@ -62,6 +39,8 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
 
         //keyboard
         private GroupBox _rebindsContainer;
+        private Dictionary<InputMapType, List<InputAction>> _inputActions;
+        private List<RebindButton> _rebindButtons = new();
 
         private void Awake()
         {
@@ -94,22 +73,6 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
 
             _closeBtn.RegisterCallback<ClickEvent>(OnCloseButtonClicked);
             _saveBtn.RegisterCallback<ClickEvent>(OnSaveButtonClicked);
-
-            //btnSettingsPages[SettingsPage.Settings].onClick.AddListener(OnBtnSettingsPageClick);
-            //btnSettingsPages[SettingsPage.GameSettings].onClick.AddListener(OnBtnGameSettingsPageClick);
-            //btnSettingsPages[SettingsPage.Audio].onClick.AddListener(OnBtnAudioSettingsPageClick);
-            //btnSettingsPages[SettingsPage.Video].onClick.AddListener(OnBtnVideoSettingsPageClick);
-            //btnSettingsPages[SettingsPage.Keyboard].onClick.AddListener(OnBtnKeyboardSettingsPageClick);
-
-            //resolutionsDropdown.onValueChanged.AddListener(OnResolutionsDropdownValueChanged);
-            //fullscreenToggle.onValueChanged.AddListener(OnFullscreenToggleValueChanged);
-
-            //audioSliders[BroAudioType.None]?.onValueChanged.AddListener(OnAllSliderValueChanged);
-            //audioSliders[BroAudioType.Music]?.onValueChanged.AddListener(OnMusicSliderValueChanged);
-            //audioSliders[BroAudioType.SFX]?.onValueChanged.AddListener(OnSFXSliderValueChanged);
-
-            //btnClose?.onClick.AddListener(OnCloseButtonClicked);
-            //btnSave?.onClick.AddListener(OnSaveButtonClicked);
         }
         
         private void OnDestroy()
@@ -128,50 +91,8 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
             {
                 r.OnRebindClick -= OnRebindStart;
             }
-
-
-            //btnSettingsPages[SettingsPage.Settings].onClick.RemoveListener(OnBtnSettingsPageClick);
-            //btnSettingsPages[SettingsPage.GameSettings].onClick.RemoveListener(OnBtnGameSettingsPageClick);
-            //btnSettingsPages[SettingsPage.Audio].onClick.RemoveListener(OnBtnAudioSettingsPageClick);
-            //btnSettingsPages[SettingsPage.Video].onClick.RemoveListener(OnBtnVideoSettingsPageClick);
-            //btnSettingsPages[SettingsPage.Keyboard].onClick.RemoveListener(OnBtnKeyboardSettingsPageClick);
-
-            //resolutionsDropdown.onValueChanged.RemoveListener(OnResolutionsDropdownValueChanged);
-
-            //fullscreenToggle.onValueChanged.RemoveListener(OnFullscreenToggleValueChanged);
-
-            //audioSliders[BroAudioType.None]?.onValueChanged.RemoveListener(OnAllSliderValueChanged);
-            //audioSliders[BroAudioType.Music]?.onValueChanged.RemoveListener(OnMusicSliderValueChanged);
-            //audioSliders[BroAudioType.SFX]?.onValueChanged.RemoveListener(OnSFXSliderValueChanged);
-
-            //foreach (var p in _inputActionReferences)
-            //{
-            //    p.Key?.button.onClick.RemoveAllListeners();
-            //}
-
-            //btnClose?.onClick.RemoveListener(OnCloseButtonClicked);
-            //btnSave?.onClick.RemoveListener(OnSaveButtonClicked);
         }
 
-        //private void OnBtnSettingsPageClick() => OpenSettingsPage(SettingsPage.Settings);
-        //private void OnBtnGameSettingsPageClick() => OpenSettingsPage(SettingsPage.GameSettings);
-        //private void OnBtnAudioSettingsPageClick() => OpenSettingsPage(SettingsPage.Audio);
-        //private void OnBtnVideoSettingsPageClick() => OpenSettingsPage(SettingsPage.Video);
-        //private void OnBtnKeyboardSettingsPageClick() => OpenSettingsPage(SettingsPage.Keyboard);
-        
-        //public void OpenSettingsPage(SettingsPage settingsPage)
-        //{
-        //    foreach (var p in settingsPages)
-        //    {
-        //        p.Value.SetActive(false);
-        //    }
-            
-        //    settingsPages[settingsPage].SetActive(true);
-            
-        //    btnSettingsPages[SettingsPage.Settings].gameObject
-        //        .SetActive(settingsPage != SettingsPage.Settings);
-        //}
-        
         private void OnCloseButtonClicked(ClickEvent e)
         {
             ViewModel.RequestGoToPrevScreen();
@@ -203,12 +124,6 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
                     if (p.Value[ai].name == "Zoom") continue;
                     if (p.Value[ai].type == InputActionType.Button)
                     {
-                        //var i = Instantiate(rebindButtonPrefab, scrollView.transform);
-                        //_inputActionReferences.Add(i, a);
-                        //i.BindingIndex = 0;
-
-                        //i.button.onClick.AddListener(() => OnRebindStart(a));
-
                         var rBtn = new RebindButton();
                         _rebindsContainer.Add(rBtn);
 
@@ -228,12 +143,6 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
                         {
                             if (p.Value[ai].bindings[b].isComposite) continue;
 
-                            //var i = Instantiate(rebindButtonPrefab, scrollView.transform);
-                            //_inputActionReferences.Add(i, a);
-                            //i.BindingIndex = b;
-
-                            //i.button.onClick.AddListener(() => OnRebindStart(a, i.BindingIndex));
-
                             var rBtn = new RebindButton();
                             _rebindsContainer.Add(rBtn);
 
@@ -252,22 +161,6 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
 
         private void UpdateAllKeysNames()
         {
-            //foreach (var p in _inputActionReferences)
-            //{
-            //    if (p.Value.type == InputActionType.Button)
-            //    {
-            //        p.Key.text.text 
-            //            = $"{p.Value.name} " +
-            //             $"- {InputControlPath.ToHumanReadableString(p.Value.bindings[p.Key.BindingIndex].effectivePath)}";
-            //    }
-            //    else if (p.Value.type == InputActionType.Value)
-            //    {
-            //        p.Key.text.text 
-            //            = $"{p.Value.bindings[p.Key.BindingIndex].name} " +
-            //             $"- {InputControlPath.ToHumanReadableString(p.Value.bindings[p.Key.BindingIndex].effectivePath)}";
-            //    }
-            //}
-
             foreach (var rBtn in _rebindButtons)
             {
                 var a = _inputActions[rBtn.inputMapType][rBtn.inputActionId];
@@ -302,27 +195,11 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
                     break;
             }
 
-            //if (type == BroAudioType.All) type = BroAudioType.None;
             Debug.Log($"[AUDIO SLIDERS] {type}: {value}");
-            //audioSliders[type].value = value;
         }
 
         private void UpdateResolutions(Resolution[] resolutions, int currentResolutionIndex)
         {
-            //resolutionsDropdown.ClearOptions();
-            //var optionsText = new List<string>();
-
-
-            //for (var i=0; i<resolutions.Length; i++)
-            //{
-            //    var option = $"{resolutions[i].width}x{resolutions[i].height} {resolutions[i].refreshRateRatio}Hz";
-            //    optionsText.Add(option);
-            //}
-
-            //resolutionsDropdown.AddOptions(optionsText);
-            //resolutionsDropdown.value = currentResolutionIndex;
-
-
             _resolutionNames.Clear();
 
             foreach (var resolution in resolutions)
@@ -339,7 +216,6 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
         {
             _fullscreenToggle.value = value;
         }
-        
         
         private void OnAllSliderValueChanged(ChangeEvent<float> e)
         {
@@ -366,7 +242,6 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
             ViewModel.RequestChangeFullscreen(e.newValue);
         }
 
-
         private void OnRebindStart(int inputActionIndex, int targetIndex, InputMapType type = InputMapType.Gameplay)
         {
             Debug.Log($"[REBINDS] OnRebindStart {inputActionIndex}, {targetIndex}");
@@ -381,6 +256,5 @@ namespace Game.MainMenu.View.UI.ScreenOptionsMenu
             _rebindBlockImage.visible = false;
             UpdateAllKeysNames();
         }
-        
     }
 }
