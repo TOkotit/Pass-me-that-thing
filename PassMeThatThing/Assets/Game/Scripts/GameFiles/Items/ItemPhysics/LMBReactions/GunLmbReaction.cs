a7bb67250cc2cbfc7b2f54f81e208c6011e56872
Sp1ke <97825185+SpikeTheSpider@users.noreply.github.com>
2026-07-30 22:41:20 +0500

    небольшие изменения ружья

--- PassMeThatThing/Assets/Game/Scripts/GameFiles/Items/ItemPhysics/LMBReactions/GunLmbReaction.cs
+++ PassMeThatThing/Assets/Game/Scripts/GameFiles/Items/ItemPhysics/LMBReactions/GunLmbReaction.cs
@@ -14,15 +14,18 @@ namespace Game.Scripts.GameFiles.Items.ItemPhysics
         [SerializeField] private float force;
         [SerializeField] private int toughnessDamage;
         [SerializeField] private TracerController tracerController;
+        private int ammo = 7;
         private float lastShotTime;
         private float delay = 1f;
         public override void Act()
         { 
-            if (Time.time - lastShotTime <= delay) return;
+            if (Time.time - lastShotTime <= delay || ammo <= 0) return;
             Debug.Log(tracerController + " " + barrel);
             physicsApplyer.ShotRaycast(barrel.position, barrel.forward, maxDistance, layersToShot,
                 force:force, damage:damage, toughDamage: toughnessDamage );
             tracerController.Shoot(barrel.position, barrel.forward);
+            lastShotTime = Time.time;
+            ammo -= 1;
         }
     }
 }
