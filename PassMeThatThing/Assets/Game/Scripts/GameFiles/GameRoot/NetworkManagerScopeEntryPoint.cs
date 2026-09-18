2c478263b3358427f9cf3e2385226c5647ab9234
zyuzyakin <zyuzyakinIvan06@gmail.com>
2026-09-18 23:58:23 +0500

    Revert "fix"
    This reverts commit 6a63d378ad8ae91c23fea4898995c98ac17bb016.

--- PassMeThatThing/Assets/Game/Scripts/GameFiles/GameRoot/NetworkManagerScopeEntryPoint.cs
+++ PassMeThatThing/Assets/Game/Scripts/GameFiles/GameRoot/NetworkManagerScopeEntryPoint.cs
@@ -1,6 +1,13 @@
-﻿using Mirror;
+﻿using Game.MainMenu.View.UI;
+using Mirror;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Systems;
 using UIRoot;
+using UnityEditor.EditorTools;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
